54646272f22a85f7eb1864f6b64c4237d81df295
vadimiam <vadym.platanyi@nure.ua>
2022-10-28 23:08:46 +0300

    Add  matrix base

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Складання матриці з числом зі строковим розподілом по стовпцях.
 Console.WriteLine("Hello, World!");
+
+internal class Matrix<T>{
+    private T[,] _matrixField;
+    public Matrix(uint RowCount = 0, uint ColumnCount = 0){
+        _matrixField = new T[RowCount, ColumnCount];
+    }
+
+    public Matrix(Matrix<T> CopyMatrix){
+        this._matrixField = CopyMatrix.ToArray();
+    }
+
+   public T this[uint i, uint j]
+   {
+      get { return _matrixField[i, j]; }
+      set { _matrixField[i, j] = value; }
+   }
+
+   public T[,] ToArray(){
+        return _matrixField;
+   }
+}
