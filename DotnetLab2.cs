94049cfbf796d494bc85d57f4ab00ad771a4c55c
vadimiam <vadym.platanyi@nure.ua>
2022-11-11 09:59:53 +0200

    Add db model

--- DotnetLab2.cs
+++ DotnetLab2.cs
@@ -1,5 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
 class Lab2 : UserCustom.Lab{
     public void Entry(){
-        
-    } 
+
+    }
+}
+
+
+
+public class SomeContext :DbContext{
+    public string DbPath { get; }
+
+    public DbSet<SomeInfo> Info { get; set; }
+
+    public SomeContext(){
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "Some.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options){
+        options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class SomeInfo
+    {
+        public int SomeValue { get; set; }
+    }
+
 }
